46c8e9a28f8b50afa47b512cc9b0123b7151583f
ishraqkhan12 <ishraqkhan645@gmail.com>
2025-05-02 00:23:20 +0500

    Update CORS settings for frontend domain

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ dotenv.config()
 connectDb()
 
 //middleware
-app.use(cors())
+app.use(cors({
+    origin: ['https://hackathone-mern-todo-frontend.vercel.app'],
+    credentials: true,
+  }));
 app.use(express.json())
 app.use(morgan('dev'))
 
